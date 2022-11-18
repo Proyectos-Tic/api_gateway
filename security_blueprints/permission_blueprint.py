50f2b7d3a609aefd6ab8cb7057e0d178b2708b09
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-18 18:09:26 -0500

    Basic version of security blueprints in ApiGateway

---
+++ security_blueprints/permission_blueprint.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request
+import requests
+from utils import HEADERS, load_file_config
+
+permission_blueprint = Blueprint("permission_blueprint",__name__)
+data_config = load_file_config()
+url_base = data_config.get('url-backend-security') + "/permission"
+
+@permission_blueprint.route("/permission/all", methods=["GET"])
+def get_all_permissions() -> dict:
+    url = url_base + "/all"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
+@permission_blueprint.route("/permission/<int:id_>", methods=["GET"])
+def get_permission_by_id(id_ : int) -> dict:
+    url = url_base + f'/{id_}'
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
+@permission_blueprint.route("/permission/create", methods=["POST"])
+def create_permission() -> dict:
+    permission = request.get_json()
+    url = url_base + f'/create'
+    response = requests.post(url, headers=HEADERS, json=permission)
+    return response.json()
+
+@permission_blueprint.route("/permission/update/<int:id_>", methods=["POST"])
+def update_permission(id_ : int) -> dict:
+    permission = request.get_json()
+    url = url_base + f'/update/{id_}'
+    response = requests.patch(url, headers=HEADERS, json=permission)
+    return response.json()
+
+@permission_blueprint.route("/permission/delete/<int:id_>", methods=["DELETE"])
+def delete_permission( id_ : int) -> dict:
+    url = url_base + f'/delete/{id_}'
+    response = requests.delete(url, headers=HEADERS)
+    return response.json()
---
+++ security_blueprints/rol_blueprint.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request
+import requests
+from utils import HEADERS, load_file_config
+
+rol_blueprint = Blueprint("rol_blueprint",__name__)
+data_config = load_file_config()
+url_base = data_config.get('url-backend-security') + "/rol"
+
+@rol_blueprint.route("/rol/all", methods=["GET"])
+def get_all_rol() -> dict:
+    url = url_base + "/all"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
+@rol_blueprint.route("/rol/<int:id_>", methods=["GET"])
+def get_rol_by_id(id_ : int) -> dict:
+    url = url_base + f'/{id_}'
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
+@rol_blueprint.route("/rol/create", methods=["POST"])
+def create_rol() -> dict:
+    rol = request.get_json()
+    url = url_base + f'/create'
+    response = requests.post(url, headers=HEADERS, json=rol)
+    return response.json()
+
+@rol_blueprint.route("/rol/update/<int:id_>", methods=["POST"])
+def update_rol(id_ : int) -> dict:
+    rol = request.get_json()
+    url = url_base + f'/update/{id_}'
+    response = requests.patch(url, headers=HEADERS, json=rol)
+    return response.json()
+
+@rol_blueprint.route("/rol/delete/<int:id_>", methods=["DELETE"])
+def delete_rol( id_ : int) -> dict:
+    url = url_base + f'/delete/{id_}'
+    response = requests.delete(url, headers=HEADERS)
+    return response.json()
